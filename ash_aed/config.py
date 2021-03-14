ac22743dbed20c8362260ace89a90b69175cb80a
Hiroki Takeda <takedahiroki@gmail.com>
2021-03-14 12:43:03 +0900

    fixed.

--- ash_aed/config.py
+++ ash_aed/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    DATABASE_URL = os.environ.get("ASH_AED_DB_URL")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
     OPENDATA_URL = (
         "https://www.city.asahikawa.hokkaido.jp/kurashi/311/316/d053328_d/fil/"
         + "012041_aed_location.csv"
